98ed38250e3d691f89fb60e3fd4e00ec080de60e
Henrique de Lima Pessoa <Henrique@MacBook-Pro-de-Felipe.local>
2022-09-06 23:13:33 -0300

    as

--- src/User/Model/index.js
+++ src/User/Model/index.js
@@ -1,6 +1,7 @@
 import mongoose from "mongoose";
 
 const UserSchema = new mongoose.Schema({
+  _id: { type: String, require: true, unique: true },
   username: { type: String, require: true, unique: true },
   password: { type: String, require: true },
   perfil: { type: String, require: true },
